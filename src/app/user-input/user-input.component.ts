d6b89f5877785c2540c5607f8cd8e8389755b1ec
Justine Boyer <160619295+justinetboyer@users.noreply.github.com>
2025-04-08 07:43:38 +0200

    updates

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '../investment-input';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  initialInvestment = '0';
+  annualInvestment = '10';
+  expectedReturn = '0';
+  duration = '0';
 
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+
+  onSubmit() {
+    console.log(
+      'Form submitted!',
+      this.initialInvestment,
+      this.annualInvestment,
+      this.expectedReturn,
+      this.duration
+    );
+    this.calculate.emit({
+      initialInvestment: +this.initialInvestment,
+      annualInvestment: +this.annualInvestment,
+      expectedReturn: +this.expectedReturn,
+      duration: +this.duration,
+    });
+  }
 }
